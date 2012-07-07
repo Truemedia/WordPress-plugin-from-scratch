437938d677cb79fa20c247dcb167645e319dd1ef
Wade Penistone <truemedia@mediacityonline.net>
2012-07-07 19:05:35 +0100

    all code/files typed in the part 1 tutorials

---
+++ dbsync.php
@@ -0,0 +1,14 @@
+<?php
+/**
+	* @package Dbsync
+	* @version 1.0
+	*/
+/*
+Plugin Name: DB Sync
+Plugin URI: http://mediacityonline.net
+Description: This is a test plugin created by me
+Author: Wade Penistone
+Version 1.0
+Author URI: http://mediacityonline.net
+*/
+?>
